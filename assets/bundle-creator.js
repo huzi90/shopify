525ae764d85d3365860ecadf3750dc4caeec9f0b
Huzi90 <fromhuzi90@gmail.com>
2025-07-08 07:34:42 -0700

    fixes

--- assets/bundle-creator.js
+++ assets/bundle-creator.js
@@ -23,7 +23,7 @@
         .then(() => {
           return fetch('/?sections=cart-drawer,cart-icon-bubble');
         })
-        .then(res => res.sections())
+        .then(res => res.JSON())
         .then((sections) => {
           console.log("Fetched sections:", sections);
           const cartDrawer = document.querySelector('cart-drawer');
