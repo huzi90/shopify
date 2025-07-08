0b6c6162cd9b871d1e726310eb43da774a18d699
Huzi90 <fromhuzi90@gmail.com>
2025-07-08 04:22:54 -0700

    add to cart functionality

--- assets/bundle-creator.js
+++ assets/bundle-creator.js
@@ -1,10 +1,27 @@
 (() => {
   document.addEventListener("DOMContentLoaded", () => {
-    const container = document.getElementById("bundleCreator")
-    const productId = document.querySelector('[data-products]')
     const Btn = document.querySelector(".bundleBtn")
     Btn.addEventListener("click",()=>{
-        console.log(productId);
+        const productId = document.getElementById("bundleCreator").dataset.products
+        fetch('/cart/add.js', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      id: productId,
+      quantity: 1
     })
-  });
-})();
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log('Added to cart:', data);
+    alert("Product added to cart!");
+  })
+  .catch(error => {
+    console.error('Error adding to cart:', error);
+    alert("There was a problem adding the product to the cart.");
+  })
+    })
+  })
+})()
