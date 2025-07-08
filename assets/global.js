b65e82d6a290b1ef79d28dceadea3f99c7269c06
Huzi90 <fromhuzi90@gmail.com>
2025-07-08 06:51:04 -0700

    global js

--- assets/global.js
+++ assets/global.js
@@ -1,4 +1,5 @@
 function getFocusableElements(container) {
+  if (!container) return []; 
   return Array.from(
     container.querySelectorAll(
       "summary, a[href], button:enabled, [tabindex]:not([tabindex^='-']), [draggable], area, input:not([type=hidden]):enabled, select:enabled, textarea:enabled, object, iframe"
